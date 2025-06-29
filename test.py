9d897e91eef694f2e7ee43090d0a448e726ea938
Outfoxeed <letourneaumorigan@gmail.com>
2025-06-29 16:37:29 +0200

    Pushing test

---
+++ test.py
@@ -0,0 +1,47 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pyperclip
+
+# Credits to https://medium.com/@ealbanez/how-to-easily-convert-latex-to-images-with-python-9062184dc815
+def latex2image(
+    latex_expression, image_name, image_size_in=(3, 0.5), fontsize=16, dpi=200
+):
+    """
+    A simple function to generate an image from a LaTeX language string.
+
+    Parameters
+    ----------
+    latex_expression : str
+        Equation in LaTeX markup language.
+    image_name : str or path-like
+        Full path or filename including filetype.
+        Accepeted filetypes include: png, pdf, ps, eps and svg.
+    image_size_in : tuple of float, optional
+        Image size. Tuple which elements, in inches, are: (width_in, vertical_in).
+    fontsize : float or str, optional
+        Font size, that can be expressed as float or
+        {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}.
+
+    Returns
+    -------
+    fig : object
+        Matplotlib figure object from the class: matplotlib.figure.Figure.
+
+    """
+
+    fig = plt.figure(figsize=image_size_in, dpi=dpi)
+    text = fig.text(
+        x=0.5,
+        y=0.5,
+        s=latex_expression,
+        horizontalalignment="center",
+        verticalalignment="center",
+        fontsize=fontsize,
+    )
+
+    plt.savefig(image_name)
+
+    return fig
+
+latex2image(r"$e^{i\pi}+1=0$", "test.svg")
+pyperclip.copy
